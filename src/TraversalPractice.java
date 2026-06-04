f442313829b6e4982f6b7365a9cbdfc0e660d7b9
Paris <203000868+pdunmire-pcd@users.noreply.github.com>
2026-06-04 10:16:08 -0700

    finished the problems in class - printNegativeNodesInOrder and sumLeafNodes

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -10,7 +10,14 @@ public class TraversalPractice {
      * @param node the root of the tree
      */
     public static void printNegativeNodesInOrder(TreeNode node) {
-
+        if (node == null) {
+            return;
+        }
+        printNegativeNodesInOrder(node.left);
+        if (node.data < 0){
+            System.out.println(node.data);
+        }
+        printNegativeNodesInOrder(node.right);
     }
     
     /**
@@ -37,6 +44,12 @@ public class TraversalPractice {
      * @return the sum of the leaf node values
      */
     public static int sumLeafNodes(TreeNode node) {
-        return -1;
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null){
+            return node.data;
+        }
+        return sumLeafNodes(node.left) + sumLeafNodes(node.right);
     }
 }
